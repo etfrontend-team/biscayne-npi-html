d86d72d97646d74122f1b08b61b120ceebf735b2
crea8ivedev <frontendteam.encircle@gmail.com>
2026-09-14 19:24:42 +0530

    home page in-progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
 import { initHeader } from "./header.js";
 import { initFooterAccordion } from "./footer.js";
 import { initPopularAdventures } from "./popular-adventures.js";
+import { initThreeCardWrapper } from "./three-card-wrapper.js";
+import { initSearchBar } from "./search-bar.js";
 
 try { initHeader(); } catch (e) { console.error('[header]', e); }
 try { initFooterAccordion(); } catch (e) { console.error('[footer]', e); }
 try { initPopularAdventures(); } catch (e) { console.error('[popular-adventures]', e); }
+try { initThreeCardWrapper(); } catch (e) { console.error('[three-card-wrapper]', e); }
+try { initSearchBar(); } catch (e) { console.error('[search-bar]', e); }
